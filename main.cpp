fa1382941ae62383ef4d5540e2551f3eae1b7d81
zishun zhou <ZhouZishun@mail.zzshub.cn>
2024-10-23 13:42:04 +0800

    merge linux build code, now this project can be build on both windows and linux

--- main.cpp
+++ main.cpp
@@ -9,14 +9,13 @@
 #include "UI/widget/CustomSplashScreen.h"
 #include "Utils/Settings/SettingsHandler.h"
 #include "QSettings"
-// #include<windows.h>
 #include "UI/DataViewer/DataViewerPage.h"
 
 #include <chrono>
 #include <thread>
 int main(int argc, char* argv[])
 {
-//    SetProcessDPIAware(); // call before the main event loop
+    //    SetProcessDPIAware(); // call before the main event loop
 #if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
@@ -27,7 +26,7 @@ int main(int argc, char* argv[])
 
     QApplication a(argc, argv);
 
-    CustomSplashScreen* screen =new CustomSplashScreen(QPixmap(":/logo/Image/splash_screen.png"));
+    CustomSplashScreen* screen = new CustomSplashScreen(QPixmap(":/logo/Image/splash_screen.png"));
     screen->show();
     a.setWindowIcon(QIcon(":/logo/Image/ProgramIcon.ico"));
     a.processEvents();
@@ -35,11 +34,10 @@ int main(int argc, char* argv[])
     //dknt的提议
     if (ZSet->isVIP())
     {
-        // Sleep(1000); //这个地方先这样，让用户能充分看到我们的启动界面，等以后加钱再改
         std::this_thread::sleep_for(std::chrono::seconds(1));
-    } else {
-      // Sleep(3000);
-      std::this_thread::sleep_for(std::chrono::seconds(3));
+    }
+    else {
+        std::this_thread::sleep_for(std::chrono::seconds(3));
     }
 
     eApp->init();
